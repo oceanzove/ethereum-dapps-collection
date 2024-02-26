518e2bd8e465b5cc8c82367b09a340f7e0ad4979
oceanzove <zove888@gmail.com>
2024-02-26 21:06:28 +0500

    Создает макет калькулятора

--- src/components/Calculator/Calculator.jsx
+++ src/components/Calculator/Calculator.jsx
@@ -1,8 +1,33 @@
 import React from 'react';
-
+import styles from './Calculator.module.css';
 const Calculator = () => {
   return (
-      <> Calculator </>
+      <div className={styles.wrapper}>
+        <div className={styles.screen}>
+           <div className={styles.display}>0</div>
+        </div>
+        <div className={styles.buttonBox}>
+            <button>C</button>
+            <button>+-</button>
+            <button>%</button>
+            <button>/</button>
+            <button>7</button>
+            <button>8</button>
+            <button>9</button>
+            <button>X</button>
+            <button>4</button>
+            <button>5</button>
+            <button>6</button>
+            <button>-</button>
+            <button>1</button>
+            <button>2</button>
+            <button>3</button>
+            <button>+</button>
+            <button>0</button>
+            <button>.</button>
+            <button>=</button>
+        </div>
+      </div>
   )
 }
 
