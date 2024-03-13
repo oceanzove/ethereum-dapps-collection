48167393b9c90bbd15f626f4e3e569bfbf49ae17
oceanzove <zove888@gmail.com>
2024-03-13 15:56:45 +0500

    Реализует scroll для списка адресов

--- src/components/Address/AddressItem.jsx
+++ src/components/Address/AddressItem.jsx
@@ -4,7 +4,7 @@ import styles from './AddressItem.module.css';
 const AddressItem = (props) => {
     return(
       <div className={styles.container}>
-          Index: {props.index.toString()} {props.address}
+          {props.index.toString()}: {props.address}
       </div>
   )
 }
