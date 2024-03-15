0119123af430216887d6b6184b8de73e366a03a5
oceanzove <zove888@gmail.com>
2024-03-15 22:22:32 +0500

    Изменяет контракт для работы с кубиками

--- src/solidity-contracts/contracts/Dice.sol
+++ src/solidity-contracts/contracts/Dice.sol
@@ -2,15 +2,18 @@
 pragma solidity >=0.4.22 <0.9.0;
 
 contract Dice {
-    function getRandomNumber(uint number) public view returns (uint) {
-        uint roll = uint(keccak256(abi.encodePacked(block.timestamp, msg.sender, number))) % 10;
-        roll = roll + 2;
-        return (roll);
+    function getRandomNumbers() public view returns (uint[2] memory) {
+        uint[2] memory result;
+        for (uint i = 0; i < 2; i++) {
+            uint roll = uint(keccak256(abi.encodePacked(block.timestamp, msg.sender, i))) % 6;
+            result[i] = roll + 1; // изменено с 2 на 1, чтобы числа были от 1 до 6
+        }
+        return result;
     }
 
-    function Winner() public view returns(uint[2] memory) {
-        uint player1 = getRandomNumber(0);
-        uint player2 = getRandomNumber(1);
+    function Winner() public view returns(uint[2][2] memory) {
+        uint[2] memory player1 = getRandomNumbers();
+        uint[2] memory player2 = getRandomNumbers();
         return [player1, player2];
     }
 }
