20d8be32933f0c149882fdd24b49409e101009d9
oceanzove <zove888@gmail.com>
2024-05-07 12:26:24 +0500

    Реализует создание стора и использование redux

---
+++ src/AppContainer.jsx
@@ -0,0 +1,34 @@
+import {connect} from "react-redux";
+import App from "./App";
+import {
+    addAddress, setGetIndexAddress,
+    updateGetAddress,
+    updateSetAddress
+} from "./redux/dragonFarmReducer";
+
+const mapStateToProps = (state) => {
+    return {
+        addressPage: state.addressPage
+    };
+};
+
+const mapDispatchToProps = (dispatch) => {
+    return {
+        onUpdateSetAddress: (value) => {
+            dispatch(updateSetAddress(value));
+        },
+        onUpdateGetAddress: (value) => {
+            dispatch(updateGetAddress(value));
+        },
+        onSetGetIndexAddress: (value) => {
+            dispatch(setGetIndexAddress(value));
+        },
+        onAddAddress: (index, address) => {
+            dispatch(addAddress(index, address));
+        }
+    };
+};
+
+const AppContainer = connect(mapStateToProps, mapDispatchToProps)(App);
+
+export default AppContainer;
