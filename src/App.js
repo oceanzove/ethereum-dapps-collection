11edce36e322c5e3dcfc815fd406b0eb41fda614
oceanzove <zove888@gmail.com>
2024-05-07 10:40:12 +0500

    Реализует черновую верстку

--- src/App.js
+++ src/App.js
@@ -1,10 +1,72 @@
 import './App.css';
+import DragonFarmContract from "./components/Contracts/DragonFarmContract";
+import {useState} from "react";
 
 function App() {
-  return (
-    <div className="App">
+    const [isSetColumnVisible, setIsSetColumnVisible] = useState(true);
+    const dragonFarmContract = new DragonFarmContract();
 
-    </div>
+    const onToggleButtonClicked = () => {
+        setIsSetColumnVisible(!isSetColumnVisible);
+    }
+
+  return (
+      <div className="App">
+        <div className="container">
+          <div className="title">
+            <h2>DragonFarm</h2>
+          </div>
+          <div className='wrapper'>
+            {isSetColumnVisible ? (
+                <div>
+                    <div className='input-div'>
+                        <label htmlFor='dragon-add' className='input-label'>
+                            Добавить дракона
+                        </label>
+                        <input type="text" id='dragon-add'
+                               value={null}
+                               onChange={null}
+                        />
+                    </div>
+                    <button
+                        disabled={null}
+                        onClick={null} className="button">Добавить
+                    </button>
+                    <div className='input-div'>
+                        <label htmlFor='dragon-get' className='input-label'>
+                            Получить дракона по индексу
+                        </label>
+                        <input type="text" id='dargon-get'
+                               value={null}
+                               onChange={null}
+                        />
+                    </div>
+                    <button
+                        disabled={null}
+                        onClick={null} className="button">Получить
+                    </button>
+                    <div className='input-div'>
+                        <label htmlFor='index' className='input-label'>Имя</label>
+                        <output id='index'>{null}</output>
+                    </div>
+                    <div className='input-div'>
+                        <label htmlFor='index' className='input-label'>ДНК</label>
+                        <output id='index'>{null}</output>
+                    </div>
+                </div>
+            ) : (
+                <div>
+                    <div className='addresses'>
+                        {null}
+                    </div>
+                </div>
+            )}
+          </div>
+        </div>
+        <button id="toggleButton" onClick={onToggleButtonClicked} className="button toggle">
+          {isSetColumnVisible ? 'Список драконов' : 'Добавить дракона'}
+        </button>
+      </div>
   );
 }
 
