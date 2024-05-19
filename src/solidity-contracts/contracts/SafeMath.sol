d43f2be7d04469f94392eb44a6e1a5614d4ffb55
zove <64221764+oceanzove@users.noreply.github.com>
2024-05-19 21:40:24 +0500

    Update SafeMath.sol

--- src/solidity-contracts/contracts/SafeMath.sol
+++ src/solidity-contracts/contracts/SafeMath.sol
@@ -7,7 +7,7 @@ pragma solidity >=0.4.22 <0.9.0;
 //pragma experimental ABIEncoderV2;
 
 contract Pattern {
-    function mul(uint256 a, uint256 b) internal pure returns (uint256) {
+    function multiply(uint256 a, uint256 b) internal pure returns (uint256) {
         if (a == 0) {
             return 0;
         }
