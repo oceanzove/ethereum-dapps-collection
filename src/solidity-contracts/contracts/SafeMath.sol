8f984da1cd39e096a120be37f42ff1060f68bf7f
oceanzove <zove888@gmail.com>
2024-05-20 16:02:06 +0500

    Загружает контракт SafeMath.sol

--- src/solidity-contracts/contracts/SafeMath.sol
+++ src/solidity-contracts/contracts/SafeMath.sol
@@ -6,7 +6,7 @@ pragma solidity >=0.4.22 <0.9.0;
 */
 //pragma experimental ABIEncoderV2;
 
-contract Pattern {
+contract SafeMath {
     function multiply(uint256 a, uint256 b) internal pure returns (uint256) {
         if (a == 0) {
             return 0;
@@ -15,4 +15,21 @@ contract Pattern {
         assert(c / a == b);
         return c;
     }
+
+    function divide(uint256 a, uint256 b) internal pure returns (uint256) {
+        uint256 c = a / b;
+        return c;
+    }
+
+    function subtract(uint256 a, uint256 b) internal pure returns (uint256) {
+        assert(b <= a);
+        return a - b;
+    }
+
+    function add(uint256 a, uint256 b) internal pure returns (uint256) {
+        uint256 c = a + b;
+        assert(c >= a);
+        return c;
+    }
+
 }
